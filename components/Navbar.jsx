31d4e0ef6f49eaf0c8615664f6f6d289ce4ee088
Amalkrishna A S <amalkrishna02016@gmail.com>
2022-11-05 11:37:58 +0530

    generated avatars from rollno

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -55,7 +55,7 @@ const Navbar = ({user,login,logout}) => {
     }
   return (
     <div className='h-100 w-screen  '>
-    <nav class="bg-white  border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 shadow-md w-full px-4 ">
+    <nav class="bg-white  border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 shadow-md w-full  ">
   <div class="container flex flex-wrap justify-between items-center mx-auto">
   <a href="https://flowbite.com/" class="flex items-center">
       <img src="https://upload.wikimedia.org/wikipedia/en/d/d0/National_Institute_of_Technology%2C_Calicut_Logo.png" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
@@ -74,7 +74,9 @@ const Navbar = ({user,login,logout}) => {
       
       <div class={` ${
         isUserOpened ? 'block' : 'hidden'
-      } z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600 absolute  w-40 top-6 right-24`}>
+      } z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600 absolute  w-40 top-8 right-20
+      
+      `}>
         <div class="py-3 px-4 hover:bg-gray-200 cursor-pointer" onClick={()=>router.push('/profile')}>
           <span class="block text-sm text-gray-900 dark:text-white">{
             //get name from email with first letter capital
