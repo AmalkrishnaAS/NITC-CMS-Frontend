a52ea73f7dd959eef664712792d67778d3396c9e
Amalkrishna A S <amalkrishna02016@gmail.com>
2023-10-29 18:37:44 +0530

    Navbar pad

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -51,7 +51,7 @@ const Navbar = ({user,login,logout,isUserOpened,setIsUserOpened}) => {
         setIsUserOpened(!isUserOpened)
     }
   return (
-        <nav class="bg-white   border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 shadow-md w-full z-1000   ">
+        <nav class={`bg-white    border-gray-200 px-2 sm:px-4 ${ user?.role!="User"?"py-4":""} rounded dark:bg-gray-900 shadow-md w-full z-1000   `}>
   <div class="container flex flex-wrap justify-between items-center mx-auto">
   <a href="https://www.nitc.ac.in" class="flex items-center">
       <img src="/favicon.png" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
